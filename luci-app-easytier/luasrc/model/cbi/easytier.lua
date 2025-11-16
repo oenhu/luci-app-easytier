366aadff13b38bc723aa48dc620ad09a702ff739
oenhu <i@untu.info>
2025-11-17 02:03:47 +0800

    Update easytier.lua
     Beginner Tutorial 菜鸟教程笔记已永久停止更新地址失效

--- luci-app-easytier/luasrc/model/cbi/easytier.lua
+++ luci-app-easytier/luasrc/model/cbi/easytier.lua
@@ -6,7 +6,7 @@ m.description = translate("A simple, secure, decentralized VPN solution for intr
         .. "Project URL: <a href=\"https://github.com/EasyTier/EasyTier\" target=\"_blank\">github.com/EasyTier/EasyTier</a>&nbsp;&nbsp;"
         .. "<a href=\"http://easytier.cn\" target=\"_blank\">Official Documentation</a>&nbsp;&nbsp;"
         .. "<a href=\"http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=jhP2Z4UsEZ8wvfGPLrs0VwLKn_uz0Q_p&authKey=OGKSQLfg61YPCpVQuvx%2BxE7hUKBVBEVi9PljrDKbHlle6xqOXx8sOwPPTncMambK&noverify=0&group_code=949700262\" target=\"_blank\">QQ Group</a>&nbsp;&nbsp;"
-        .. "<a href=\"https://doc.oee.icu\" target=\"_blank\">Beginner Tutorial</a>")
+
 
 m:section(SimpleSection).template  = "easytier/easytier_status"
 
